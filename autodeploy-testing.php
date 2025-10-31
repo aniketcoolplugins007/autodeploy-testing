84f3a3df0441a119df92e2639590f7f811602371
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2025-10-31 15:57:06 +0530

    Added plugin header file.
    added plugin header file.

---
+++ autodeploy-testing.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Plugin Name: Auto Deploy Testing
+ * Description: This is a testing plugin for the auto deploy feature.
+ * Version: 0.1
+ * Author: Aniket
+ * Author URI: https://aniket.com
+ * Text Domain: autodeploy-testing
+ * Domain Path: /languages
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Requires at least: 5.0
+ * Requires PHP: 7.0
+ */
+
+ echo "Hello World 0.1";
